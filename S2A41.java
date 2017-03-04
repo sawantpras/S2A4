f1597c481d80ea5f9dc3755293fde76a26fd2e4d
sawantpras <sawantpras@gmail.com>
2017-03-04 23:30:00 +0530

    Add files via upload

---
+++ S2A41.java
@@ -0,0 +1,13 @@
+public class S2A41
+{
+	public static void main(String args[])
+    {
+		int n=Integer.parseInt(args[0]);
+		if(n==0) // checking n is equal to zero
+		System.out.println("You Have Entered Zero");
+		else if(n>0) // checking n is greater than zero
+		System.out.println("You Have Entered Positive Value");
+		else
+		System.out.println("You Have Entered Negative Value");
+	}
+}
